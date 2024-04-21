20ad1f61c1e3069476fa56305b694d28908fad7c
pateli <patelistorm@gmail.com>
2024-04-21 14:10:30 +0200

    dzialajacy timer js

--- main.js
+++ main.js
@@ -1,23 +1,35 @@
-time();
-
 setInterval(time, 1000);
 function time() {
-  const teraz = new Date();
 
-  let days = document.getElementById('days');
-  let hours = document.getElementById('hours');
-  let minutes = document.getElementById('minutes');
-  let seconds = document.getElementById('seconds');
+let days_p = document.getElementById('days');
+let hours_p = document.getElementById('hours');
+let minutes_p = document.getElementById('minutes');
+let seconds_p = document.getElementById('seconds');
 
-  days.textContent = teraz.getDay();
-  hours.textContent = teraz.getHours();
+const teraz = new Date();
+let rok = teraz.getFullYear();
+const terazMili = teraz.getTime();
 
-  const minuta = teraz.getMinutes();
-  let fixedMinuta = minuta < 10 ? `0${minuta}` : minuta;
+const KoniecRoku = new Date(`June 21, ${rok} 09:00:00`).getTime();
 
-  const sekunda = teraz.getSeconds();
-  let fixedSekunda = sekunda < 10 ? `0${sekunda}` : sekunda;
+//roznica w milisek. miedzy teraz a koncem roku
+let diff = Math.abs(KoniecRoku - terazMili);
 
-  minutes.textContent = fixedMinuta;
-  seconds.textContent = fixedSekunda;
+
+
+const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+const fixedDays = (days < 10) ? '0' + days : days;
+const fixedHours = (hours < 10) ? '0' + hours : hours;
+const fixedMinutes = (minutes < 10) ? '0' + minutes : minutes;
+const fixedSeconds = (seconds < 10) ? '0' + seconds : seconds;
+
+days_p.textContent = fixedDays;
+hours_p.textContent = fixedHours;
+minutes_p.textContent = fixedMinutes;
+seconds_p.textContent = fixedSeconds  ;
 }
+time();
